f04d98cf4dd3f9c1daa9d625d98042bc2654dda8
Arpita-Jaiswal <Arpita-Jaiswal@users.noreply.github.com>
2023-04-11 22:01:34 +0000

    deploy: ed07c0c1dc45686a5e6d75ec053ef3a5b255366b

---
+++ -/amitu.github.io/dotcom/rive.js
@@ -0,0 +1,47 @@
+/*import "https://unpkg.com/@rive-app/canvas@1.0.102";
+
+
+window.onload = (e) => {
+    let old_node = document.getElementById("panda-image");
+    let new_node  = stringToHTML("<canvas id=\"canvas\"></canvas>");
+    old_node.parentNode.replaceChild(new_node.children[0], old_node);
+    const r = new rive.Rive({
+        // src: "https://cdn.rive.app/animations/vehicles.riv",
+        src: "panda.riv",
+        canvas: document.getElementById("canvas"),
+        autoplay: true,
+        stateMachines: "State Machine 1",
+        onLoad: () => {
+            r.resizeDrawingSurfaceToCanvas();
+        },
+    });
+}*/
+function onScriptLoad_js() {
+    let old_node = document.getElementById("panda-image");
+    let new_node  = stringToHTML("<canvas id=\"canvas\"></canvas>");
+    old_node.parentNode.replaceChild(new_node.children[0], old_node);
+
+    const r = new window.rive.Rive({
+        src: "panda.riv",
+        canvas: document.getElementById("canvas"),
+        autoplay: true,
+        stateMachines: "State Machine 1",
+        onLoad: () => {
+            r.resizeDrawingSurfaceToCanvas();
+        },
+    });
+}
+
+
+function animate_panda() {
+    let old_node = document.getElementById("panda-image");
+    let new_node  = stringToHTML("<canvas id=\"canvas\"></canvas>");
+    old_node.parentNode.replaceChild(new_node.children[0], old_node)
+}
+
+
+function stringToHTML(str) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(str, 'text/html');
+    return doc.body;
+}
